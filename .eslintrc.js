bb9c3967ba7b1685f26a098e219ed89f554661d0
Kilderson Sena <dersonsena@gmail.com>
2020-03-27 17:26:05 -0300

    feat: item 3 - Setup of the eslint

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true
+  },
+  extends: [
+    "airbnb",
+    "react-app",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "prettier",
+    "prettier/react"
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["react", "import", "jsx-a11y"],
+  rules: {
+    "global-require": "off",
+    "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "react/jsx-filename-extension": [
+      "error",
+      {
+        extensions: [".tsx"]
+      }
+    ],
+    "react/no-unused-prop-types": ["warn"],
+    "react/jsx-boolean-value": "off",
+    "react/forbid-prop-types": "off",
+    "import/prefer-default-export": "off",
+    "import/extensions": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-member-accessibility": "off"
+  },
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"]
+    },
+    "import/resolver": {
+      typescript: {}
+    }
+  }
+};
